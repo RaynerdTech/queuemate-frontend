b2a97388954ac1ca3b1144d0f2302d901366c5e9
RaynerdTech <raynerdtech@gmail.com>
2025-12-28 11:39:14 +0100

    spaces

--- src/screens/AuthScreen.tsx
+++ src/screens/AuthScreen.tsx
@@ -97,7 +97,7 @@ export default function AuthScreen() {
           >
             QueueMate
           </Text>
-          <Text 
+          <Text     
             style={styles.heroSubtitle}
             accessible={true}
           >
